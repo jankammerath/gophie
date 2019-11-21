48051d3ed0d8774ae5f55c20d25fb3bbb50a1e71
Jan Kammerath <jankammerath@googlemail.com>
2019-11-21 22:34:44 +0100

    removed unnecessary libs from import

--- src/ui/PageView.java
+++ src/ui/PageView.java
@@ -1,12 +1,8 @@
 package ui;
 
 import java.awt.*;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 import javax.swing.*;
