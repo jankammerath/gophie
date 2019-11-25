9db942fcb20d856e98f84d3f5383dd6aa34f6be6
Jan Kammerath <jankammerath@googlemail.com>
2019-11-25 09:51:49 +0100

    added hover effect to download window buttons

--- src/org/gophie/ui/DownloadWindow.java
+++ src/org/gophie/ui/DownloadWindow.java
@@ -1,6 +1,7 @@
 package org.gophie.ui;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
@@ -9,6 +10,11 @@ import org.gophie.net.*;
 import org.gophie.net.event.*;
 
 public class DownloadWindow{
+    private static final String ACTIONBAR_BACKGROUND = "#248AC2";
+    private static final String ACTIONBAR_TEXTCOLOR = "#ffffff";
+    private static final String ACTIONBAR_INACTIVE_TEXTCOLOR = "#76bce3";
+    private static final String FILELIST_BACKGROUND = "#1b1b1b";
+
     /* local objects */
     private DownloadList list;
     private DownloadItem[] data;
@@ -43,7 +49,7 @@ public class DownloadWindow{
         this.fileListView.setCellRenderer(new DownloadItemRenderer());
         this.fileListView.setFixedCellWidth(this.fileListView.getWidth());
         this.fileListView.setOpaque(true);
-        this.fileListView.setBackground(Color.decode("#1b1b1b"));
+        this.fileListView.setBackground(Color.decode(FILELIST_BACKGROUND));
 
         JScrollPane listScrollPane = new JScrollPane(this.fileListView);
         listScrollPane.setOpaque(false);
@@ -63,10 +69,13 @@ public class DownloadWindow{
 
         this.actionBar.setLayout(new BorderLayout());
         this.actionBar.setBorder(new EmptyBorder(8,16,10,16));
-        this.actionBar.setBackground(Color.decode("#248AC2"));
+        this.actionBar.setBackground(Color.decode(ACTIONBAR_BACKGROUND));
         this.actionBar.add(this.clearButton,BorderLayout.EAST);
         this.actionBar.add(this.actionButton,BorderLayout.WEST);
         this.frame.add(this.actionBar, BorderLayout.SOUTH);
+
+        /* hide the action button for empty lists */
+        this.actionButton.setVisible(false);
 
         /* update the list for the first time */
         this.updateList();
@@ -79,18 +88,42 @@ public class DownloadWindow{
         result.setCursor(new Cursor(Cursor.HAND_CURSOR));
         result.setOpaque(false);
 
+        /* icon for the button using the icon font */
         JLabel iconLabel = new JLabel(iconText);
         iconLabel.setBorder(new EmptyBorder(0,0,0,6));
         iconLabel.setOpaque(false);
         iconLabel.setFont(ConfigurationManager.getIconFont(14f));
-        iconLabel.setForeground(Color.decode("#ffffff"));
+        iconLabel.setForeground(Color.decode(ACTIONBAR_INACTIVE_TEXTCOLOR));
         result.add(iconLabel,BorderLayout.WEST);
 
+        /* text for the button using the default text font */
         JLabel textLabel = new JLabel(text);
         textLabel.setOpaque(false);
         textLabel.setFont(ConfigurationManager.getDefaultFont(12f));
-        textLabel.setForeground(Color.decode("#ffffff"));
+        textLabel.setForeground(Color.decode(ACTIONBAR_INACTIVE_TEXTCOLOR));
         result.add(textLabel,BorderLayout.EAST);
+        result.addMouseListener(new MouseAdapter() {
+            /* notify the listeners of the forward move request */
+            public void mouseReleased(MouseEvent evt){
+                /* will be handled by another handler */
+            }
+
+            /* set the color to the hover color and use the hand cursor */
+            public void mouseEntered(MouseEvent evt) {
+                iconLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                iconLabel.setForeground(Color.decode(ACTIONBAR_TEXTCOLOR));
+                textLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                textLabel.setForeground(Color.decode(ACTIONBAR_TEXTCOLOR));
+            }
+        
+            /* revert back to the default cursor and default color */
+            public void mouseExited(MouseEvent evt) {
+                iconLabel.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+                iconLabel.setForeground(Color.decode(ACTIONBAR_INACTIVE_TEXTCOLOR));
+                textLabel.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+                textLabel.setForeground(Color.decode(ACTIONBAR_INACTIVE_TEXTCOLOR));
+            }
+        });
 
         return result;
     }
