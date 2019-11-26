9e9c8530dd61fcf2a08d964cf21b6bed62bff01a
Jan Kammerath <jankammerath@googlemail.com>
2019-11-26 10:57:12 +0100

    added gpl license header to all code files that fall under the GPLv3

--- src/org/gophie/ui/event/ActionButtonEventListener.java
+++ src/org/gophie/ui/event/ActionButtonEventListener.java
@@ -1,3 +1,21 @@
+/*
+    This file is part of Gophie.
+
+    Gophie is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gophie is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gophie. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 package org.gophie.ui.event;
 
 public interface ActionButtonEventListener {
